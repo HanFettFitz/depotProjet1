e1729fb9d87d8b887b64813206c6ff25f945b440
Fabien CADIC <fabien.cadic2021@campus-eni.fr>
2022-02-14 12:15:05 +0100

    i

--- src/main/java/fr/eni/encheres/ihm/AcceuilServlet.java
+++ src/main/java/fr/eni/encheres/ihm/AcceuilServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class AcceuilServlet
  */
-@WebServlet({"", "/", "/index.html", "/Acceuil"})
+@WebServlet({"", "/", "/index.html", "/acceuil"})
 public class AcceuilServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
